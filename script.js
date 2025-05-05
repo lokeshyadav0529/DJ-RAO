5a05723a452cd21ad88eedc538586ae013f305b5
LOKESH KUMAR <lokeshyadav05299@gmail.com>
2025-05-05 17:00:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Welcome to DJ YADAV");
 
 // Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio(songs[songIndex].filePath);
+let audioElement = new Audio('0.1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
