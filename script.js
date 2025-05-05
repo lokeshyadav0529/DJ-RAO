c70bfb7beba11cac166a303a8072bd2764752913
LOKESH KUMAR <lokeshyadav05299@gmail.com>
2025-05-05 15:00:18 +0530

    Update script.js
    Signed-off-by: LOKESH KUMAR <lokeshyadav05299@gmail.com>

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Kabootar Haryanvi song ", filePath: "1.mp3", coverPath: "1.png"},
+    {songName: "Kabootar Haryanvi song ", filePath: '1.mp3', coverPath: "1.png"},
     {songName: "4G KA JAMANA", filePath: "2.mp3", coverPath: "2.jpeg"},
     {songName: "Bahu Chaudhariya ki", filePath: "3.mp3", coverPath: "3.jpeg"},
     {songName: "Bahu Jamidar ki  New  2015 Superhit ", filePath: "4.mp3", coverPath: "4.jpeg"},
@@ -18,7 +18,7 @@ let songs = [
     {songName: "BAHU_BATASE_Si_", filePath: "6.mp3", coverPath: "6.jpeg"},
     {songName: "Bandook Chalegi Sapna Chaudhary New Super Hit Song", filePath: "7.mp3", coverPath: "7.jpeg"},
     {songName: "BANDOOK : Pranjal Dahiya ", filePath: "8.mp3", coverPath: "8.jpeg"},
-    {songName: "Bhagatt Aadmi ", filePath: "https://lokeshyadav0529.github.io/DJ-RAO/9.mp3", coverPath: "9.jpeg"},
+    {songName: "Bhagatt Aadmi ", filePath: '9.mp3', coverPath: "9.jpeg"},
     {songName: "Bhirad Ladgi - Masoom Sharma", filePath: "10.mp3", coverPath: "10.jpeg"},
 ]
 
