90e94a0ff52d630b433ab1e4a904ae7c1f08ae80
LOKESH KUMAR <lokeshyadav05299@gmail.com>
2025-05-06 23:47:21 +0530

    Update masoom.js

--- masoom.js
+++ masoom.js
@@ -33,8 +33,8 @@ let songs = [
 ]
 
 songItems.forEach((element, i)=>{ 
-    element.getElementsByTagName("img")[11].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[11].innerText = songs[i].songName; 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
 })
  
 
