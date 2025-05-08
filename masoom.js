d52f590d61495aeff4e3f53d99b615d827c585a1
LOKESH KUMAR <lokeshyadav05299@gmail.com>
2025-05-08 22:28:20 +0530

    Update masoom.js

--- masoom.js
+++ masoom.js
@@ -6,7 +6,7 @@ let audioElement = new Audio('11.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
-let  masterSongName = document.getElementById('SongName');
+let  masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
