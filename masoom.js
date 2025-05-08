a5ad32e000ff502892417d017c4b2e0a5cfab6f5
LOKESH KUMAR <lokeshyadav05299@gmail.com>
2025-05-08 21:52:38 +0530

    Update masoom.js

--- masoom.js
+++ masoom.js
@@ -78,7 +78,7 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
         audioElement.src = `${songIndex+1}.mp3`;
-         masterSongName.innerText = songs[songIndex].songName;
+        masterSongName.innerText = songs[songIndex].songName;
         audioElement.currentTime = 0;
         audioElement.play();
         gif.style.opacity = 1;
@@ -95,7 +95,7 @@ document.getElementById('next').addEventListener('click', ()=>{
         songIndex += 1;
     }
     audioElement.src = `${songIndex+1}.mp3`;
-     masterSongName.innerText = songs[songIndex].songName;
+    masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
     masterPlay.classList.remove('fa-play-circle');
